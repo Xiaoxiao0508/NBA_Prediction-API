f82993417bfe89c47ec5d3f632c88b5403eb4980
Xiaoxiao <cxxmelbourne@hotmail.com>
2021-04-21 21:35:33 +1000

    modify add list of player to team endpoint

--- WebApi/Controllers/PlayerSelectionController.cs
+++ WebApi/Controllers/PlayerSelectionController.cs
@@ -42,18 +42,89 @@ namespace WebApi.Controllers
         }
 
 
-        [HttpPost]
-        public async Task<bool> PostPlayer([FromBody] PlayerSelection playerSelection)
+        // [HttpPost]
+        // public async Task<bool> PostPlayer([FromBody] PlayerSelection playerSelection)
+        // {
+        //     var PlayerCount = _context.PlayerSelection.Where(p => p.TeamName == playerSelection.TeamName).CountAsync().Result;
+
+        //     if (PlayerCount < 15)
+        //     {
+        //         try
+        //         {
+        //             _context.PlayerSelection.Add(playerSelection);
+
+        //             await _context.SaveChangesAsync();
+        //         }
+        //         catch (DbUpdateException)
+        //         {
+
+        //             return false;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+
+        //     return true;
+        // }
+        // [HttpPost]
+        // public async Task<bool> PostPlayer([FromBody] PlayerSelections playerSelections)
+
+        // {
+        //     List<int> PlayerKeyList = new List<int>();
+        //     int[] list=
+        //     var selections = new PlayerSelections(playerSelections.TeamName, PlayerKeyList);
+
+        //     var PlayerCount = _context.PlayerSelection.Where(p => p.TeamName == playerSelections.TeamName).CountAsync().Result;
+
+        //     if (PlayerCount < 15)
+        //     {
+        //         try
+        //         {
+        //             foreach (int i in PlayerKeyList)
+        //             {
+        //               var selection=new PlayerSelection(playerSelections.TeamName,i);
+        //                 _context.PlayerSelection.Add(selection);
+
+        //                 await _context.SaveChangesAsync();
+        //             }
+
+        //         }
+        //         catch (DbUpdateException)
+        //         {
+
+        //             return false;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+
+        //     return true;
+        // }
+
+         [HttpPost]
+        public async Task<bool> PostPlayer([FromBody] PlayerSelections selections)
+
         {
-            var PlayerCount = _context.PlayerSelection.Where(p => p.TeamName == playerSelection.TeamName).CountAsync().Result;
+           
+
+            var PlayerCount = _context.PlayerSelection.Where(p => p.TeamName == selections.TeamName).CountAsync().Result;
 
             if (PlayerCount < 15)
             {
                 try
                 {
-                    _context.PlayerSelection.Add(playerSelection);
+                    foreach (int i in selections.PlayerKeys)
+                    {
+                      var selection=new PlayerSelection(selections.TeamName,i);
+                        _context.PlayerSelection.Add(selection);
 
-                    await _context.SaveChangesAsync();
+                        await _context.SaveChangesAsync();
+                    }
+
                 }
                 catch (DbUpdateException)
                 {
@@ -68,6 +139,7 @@ namespace WebApi.Controllers
 
             return true;
         }
+
 
         // DELETE: api/PlayerSelection/DeletePlayer
         [HttpDelete("DeletePlayer")]
