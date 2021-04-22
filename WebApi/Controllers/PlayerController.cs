c7d8bca66d11d8e7e70d6200714246dd5c8e97e1
Xiaoxiao <cxxmelbourne@hotmail.com>
2021-04-22 20:48:03 +1000

    modify search player endpoint

--- WebApi/Controllers/PlayerController.cs
+++ WebApi/Controllers/PlayerController.cs
@@ -16,20 +16,13 @@ namespace WebApi.Controllers
     public class PlayerController : ControllerBase
     {
         private readonly NBA_DBContext _context;
-            NBA_DBContext context = new NBA_DBContext();
+        // NBA_DBContext context = new NBA_DBContext();
 
 
         public PlayerController(NBA_DBContext context)
         {
             _context = context;
-            // {
 
-
-            //     var query = (from s in context.allPlayers
-            //                  join ps in context.PlayerSelection on s.Player_key equals ps.Player_key
-            //                  where ps.TeamName=="searchstring"
-            //                  select s).ToList();
-            // }
 
         }
 
@@ -71,193 +64,210 @@ namespace WebApi.Controllers
         }
 
         // GET: api/Player
-        // [Route("getPlayersFromTeam")]
-        // [HttpGet]
-        // public async Task<ActionResult<IEnumerable<Player>>> getPlayersFromTeam([FromBody] FullTeamRosterRequest teamReq)
-        // {
-        //     var validFilter = new FullTeamRosterRequest(teamReq.PageNumber, teamReq.PageSize, teamReq.SortString, teamReq.TeamName);
-        //     var userInput = teamReq.TeamName;
-        //     var pagedData = await _context.allPlayers
-        //     // .FromSqlRaw("getPlayersFromTeam @p0", userInput)
-        //         .FromSqlRaw("getPlayersFromTeam @p0", userInput)
-        //     // .OrderBy(p => EF.Property<object>(p, validFilter.SortString))
-        //     .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
-        //     .Take(validFilter.PageSize)
-        //     .ToListAsync();
-
-        //     var totalRecords = await _context.allPlayers.CountAsync();
-        //     var pagesCount = (decimal)totalRecords / (decimal)teamReq.PageSize;
-
-        //     if ((pagesCount % 1) != 0)
-        //     {
-        //         pagesCount = Decimal.ToInt32(pagesCount);
-        //         pagesCount += 1;
-        //     }
-
-        //     return Ok(new Response<List<Player>>(pagedData, Decimal.ToInt32(pagesCount)));
-        // }
-
         [Route("getPlayersFromTeam")]
         [HttpGet]
-        public List<Player> getPlayersFromTeam([FromQuery] string searchstring)
+        public async Task<ActionResult<IEnumerable<Player>>> getPlayersFromTeam([FromBody] FullTeamRosterRequest teamReq)
         {
-            var query = (from s in context.allPlayers
-                             join ps in context.PlayerSelection on s.Player_key equals ps.Player_key
-                             where ps.TeamName=="searchstring"
-                             select s).ToList();
+            var validFilter = new FullTeamRosterRequest(teamReq.PageNumber, teamReq.PageSize, teamReq.SortString, teamReq.TeamName);
+            var userInput = teamReq.TeamName;
+            var pagedData = await _context.allPlayers
+                .FromSqlRaw("getPlayersFromTeam @p0", userInput)
+            // .OrderBy(p => EF.Property<object>(p, validFilter.SortString))
+            .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+            .Take(validFilter.PageSize)
+            .ToListAsync();
 
-                return query;
+            var totalRecords = await _context.allPlayers.CountAsync();
+            var pagesCount = (decimal)totalRecords / (decimal)teamReq.PageSize;
+
+            if ((pagesCount % 1) != 0)
+            {
+                pagesCount = Decimal.ToInt32(pagesCount);
+                pagesCount += 1;
             }
 
+            return Ok(new Response<List<Player>>(pagedData, Decimal.ToInt32(pagesCount)));
+        }
 
-            // GET: api/Player
-            [HttpGet("{search}")]
 
-            public async Task<ActionResult<IEnumerable<Player>>> GetPlayer([FromQuery] SearchPaginationFilter filter)
+        [HttpGet("SearchPlayer")]
+
+        public async Task<ActionResult<IEnumerable<Player>>> GetPlayer([FromQuery] SearchPaginationFilter filter)
+        {
+
+            // partial first name and partial lastname search or player initials with pagination
+            var validFilter = new SearchPaginationFilter(filter.searchstring, filter.PageNumber, filter.PageSize, filter.SortString, filter.SortOrder);
+
+            string[]? splitString = filter.searchstring?.Split(' ');
+            var pagedData=new List<Player>();
+            decimal pagescount=0;
+
+
+            if (splitString?.Length == 2)
             {
-
-                // partial first name and partial lastname search or player initials with pagination
-                var validFilter = new SearchPaginationFilter(filter.searchstring, filter.PageNumber, filter.PageSize);
-
-                string[]? splitString = filter.searchstring?.Split(' ');
-
-
-                if (splitString?.Length == 2)
-                {
-                    //filters the player view data based on the searchstring and adds pagination based on the url.
-                    var pageData = await _context.allPlayers.Where(p => EF.Functions.Like(p.FIRSTNAME, $"{splitString[0]}%") && EF.Functions.Like(p.Lastname, $"{splitString[1]}%"))
-                        .OrderBy(p => p.FIRSTNAME)
-                        .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
-                        .Take(validFilter.PageSize)
-                        .ToListAsync();
-
-                    //total pages in current search
-                    var totalRecords = await _context.allPlayers.Where(p => EF.Functions.Like(p.FIRSTNAME, $"{splitString[0]}%") && EF.Functions.Like(p.Lastname, $"{splitString[1]}%"))
-                        .OrderBy(p => p.FIRSTNAME).CountAsync();
-
-                    var pagesCount = (decimal)totalRecords / (decimal)filter.PageSize;
-
-                    if ((pagesCount % 1) != 0)
-                    {
-                        pagesCount = Decimal.ToInt32(pagesCount);
-                        pagesCount += 1;
-                    }
-
-                    return Ok(new Response<List<Player>>(pageData, Decimal.ToInt32(pagesCount)));
-                }
-                else
-                {
-                    //Partial firstname + lastname search this code runs if the splitString array has more then 2 items
-                    var pageData = await _context.allPlayers
-                        .Where(p => EF.Functions.Like(p.FIRSTNAME, $"{filter.searchstring}%") || EF.Functions.Like(p.Lastname, $"{filter.searchstring}%") || EF.Functions.Like(p.FIRSTNAME + " " + p.Lastname, $"{filter.searchstring}%"))
-                        .OrderBy(p => p.FIRSTNAME)
-                        .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
-                        .Take(validFilter.PageSize)
-                        .ToListAsync();
-
-                    //total pages in current search
-                    var totalRecords = await _context.allPlayers.Where(p => EF.Functions.Like(p.FIRSTNAME, $"{filter.searchstring}%") || EF.Functions.Like(p.Lastname, $"{filter.searchstring}%") || EF.Functions.Like(p.FIRSTNAME + " " + p.Lastname, $"{filter.searchstring}%")).CountAsync();
-
-                    var pagesCount = (decimal)totalRecords / (decimal)filter.PageSize;
-
-                    if ((pagesCount % 1) != 0)
-                    {
-                        pagesCount = Decimal.ToInt32(pagesCount);
-                        pagesCount += 1;
-                    }
-
-
-                    return Ok(new Response<List<Player>>(pageData, Decimal.ToInt32(pagesCount)));
-                }
-            }
-
-            //Get Column Headers
-            [HttpGet("headers")]
-
-            public async Task<ActionResult<IEnumerable<ColumnHeaders>>> GetHeaders()
-            {
-                var Data = await _context.columnHeaders
+                //filters the player view data based on the searchstring and adds pagination based on the url.
+                var pageData = await _context.allPlayers.Where(p => EF.Functions.Like(p.FIRSTNAME, $"{splitString[0]}%") && EF.Functions.Like(p.Lastname, $"{splitString[1]}%"))
+                    .OrderBy(p => EF.Property<object>(p, validFilter.SortString))
+                    .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+                    .Take(validFilter.PageSize)
                     .ToListAsync();
 
-                return Ok(new Response<List<ColumnHeaders>>(Data));
-            }
+                //total pages in current search
+                var totalRecords = await _context.allPlayers.Where(p => EF.Functions.Like(p.FIRSTNAME, $"{splitString[0]}%") && EF.Functions.Like(p.Lastname, $"{splitString[1]}%"))
+                    .OrderBy(p => p.FIRSTNAME).CountAsync();
 
-            // GET: api/Player/5
-            // search player by player_key
-            [HttpGet("{Player_key}")]
-            public async Task<ActionResult<Player>> GetPlayer(int Player_key)
-            {
-                var player = await _context.allPlayers.FindAsync(Player_key);
+                var pagesCount = (decimal)totalRecords / (decimal)filter.PageSize;
 
-                if (player == null)
+                if ((pagesCount % 1) != 0)
                 {
-                    return NotFound();
+                    pagesCount = Decimal.ToInt32(pagesCount);
+                    pagesCount += 1;
                 }
+                if (filter.SortOrder == "ASC")
+                {
+                    return Ok(new Response<List<Player>>(pageData, Decimal.ToInt32(pagesCount)));
 
-                return player;
+                }
+                else if (filter.SortOrder == "DESC")
+                {
+                    pageData = await _context.allPlayers.Where(p => EF.Functions.Like(p.FIRSTNAME, $"{splitString[0]}%") && EF.Functions.Like(p.Lastname, $"{splitString[1]}%"))
+                       .OrderByDescending(p => EF.Property<object>(p, validFilter.SortString))
+                       .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+                       .Take(validFilter.PageSize)
+                       .ToListAsync();
+
+                    return Ok(new Response<List<Player>>(pageData, Decimal.ToInt32(pagesCount)));
+                }
+            }
+            else
+            {
+                //Partial firstname + lastname search this code runs if the splitString array has more then 2 items
+                var pageData = await _context.allPlayers
+                    .Where(p => EF.Functions.Like(p.FIRSTNAME, $"{filter.searchstring}%") || EF.Functions.Like(p.Lastname, $"{filter.searchstring}%") || EF.Functions.Like(p.FIRSTNAME + " " + p.Lastname, $"{filter.searchstring}%"))
+                      .OrderBy(p => EF.Property<object>(p, validFilter.SortString))
+                    .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+                    .Take(validFilter.PageSize)
+                    .ToListAsync();
+
+                //total pages in current search
+                var totalRecords = await _context.allPlayers.Where(p => EF.Functions.Like(p.FIRSTNAME, $"{filter.searchstring}%") || EF.Functions.Like(p.Lastname, $"{filter.searchstring}%") || EF.Functions.Like(p.FIRSTNAME + " " + p.Lastname, $"{filter.searchstring}%")).CountAsync();
+
+                var pagesCount = (decimal)totalRecords / (decimal)filter.PageSize;
+
+                if ((pagesCount % 1) != 0)
+                {
+                    pagesCount = Decimal.ToInt32(pagesCount);
+                    pagesCount += 1;
+                }
+                if (filter.SortOrder == "ASC")
+               
+                {
+                     pagedData=pageData;
+                     pagescount=pagesCount;
+                    return Ok(new Response<List<Player>>(pageData, Decimal.ToInt32(pagesCount)));
+
+                }
+                else if (filter.SortOrder == "DESC")
+                {
+                    pageData = await _context.allPlayers
+                    .Where(p => EF.Functions.Like(p.FIRSTNAME, $"{filter.searchstring}%") || EF.Functions.Like(p.Lastname, $"{filter.searchstring}%") || EF.Functions.Like(p.FIRSTNAME + " " + p.Lastname, $"{filter.searchstring}%"))
+                      .OrderByDescending(p => EF.Property<object>(p, validFilter.SortString))
+                    .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+                    .Take(validFilter.PageSize)
+                    .ToListAsync();
+                     return Ok(new Response<List<Player>>(pageData, Decimal.ToInt32(pagesCount)));
+                }
+               
+            }
+             return Ok(new Response<List<Player>>(pagedData, Decimal.ToInt32(pagescount)));
+        }
+        //Get Column Headers
+        [HttpGet("headers")]
+
+        public async Task<ActionResult<IEnumerable<ColumnHeaders>>> GetHeaders()
+        {
+            var Data = await _context.columnHeaders
+                .ToListAsync();
+
+            return Ok(new Response<List<ColumnHeaders>>(Data));
+        }
+
+        // GET: api/Player/5
+        // search player by player_key
+        [HttpGet("{Player_key}")]
+        public async Task<ActionResult<Player>> GetPlayer(int Player_key)
+        {
+            var player = await _context.allPlayers.FindAsync(Player_key);
+
+            if (player == null)
+            {
+                return NotFound();
             }
 
-
-            // PUT: api/Player/5
-            // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-            // [HttpPut("{id}")]
-            // public async Task<IActionResult> PutPlayer(int id, Player player)
-            // {
-            //     if (id != player.Id)
-            //     {
-            //         return BadRequest();
-            //     }
-
-            //     _context.Entry(player).State = EntityState.Modified;
-
-            //     try
-            //     {
-            //         await _context.SaveChangesAsync();
-            //     }
-            //     catch (DbUpdateConcurrencyException)
-            //     {
-            //         if (!PlayerExists(id))
-            //         {
-            //             return NotFound();
-            //         }
-            //         else
-            //         {
-            //             throw;
-            //         }
-            //     }
-
-            //     return NoContent();
-            // }
-
-            // POST: api/Player
-            // [HttpPost]
-            // public async Task<ActionResult<Player>> PostPlayer(Player player)
-            // {
-            //     _context.Players.Add(player);
-            //     await _context.SaveChangesAsync();
-
-            //     return CreatedAtAction("GetPlayer", new { id = player.Id }, player);
-            // }
-
-            // DELETE: api/Player/5
-            // [HttpDelete("{id}")]
-            // public async Task<IActionResult> DeletePlayer(int id)
-            // {
-            //     var player = await _context.Players.FindAsync(id);
-            //     if (player == null)
-            //     {
-            //         return NotFound();
-            //     }
-
-            //     _context.Players.Remove(player);
-            //     await _context.SaveChangesAsync();
-
-            //     return NoContent();
-            // }
-
-            // private bool PlayerExists(int id)
-            // {
-            //     return _context.Players.Any(e => e.Id == id);
-            // }
+            return player;
         }
+
+
+        // PUT: api/Player/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPut("{id}")]
+        // public async Task<IActionResult> PutPlayer(int id, Player player)
+        // {
+        //     if (id != player.Id)
+        //     {
+        //         return BadRequest();
+        //     }
+
+        //     _context.Entry(player).State = EntityState.Modified;
+
+        //     try
+        //     {
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (DbUpdateConcurrencyException)
+        //     {
+        //         if (!PlayerExists(id))
+        //         {
+        //             return NotFound();
+        //         }
+        //         else
+        //         {
+        //             throw;
+        //         }
+        //     }
+
+        //     return NoContent();
+        // }
+
+        // POST: api/Player
+        // [HttpPost]
+        // public async Task<ActionResult<Player>> PostPlayer(Player player)
+        // {
+        //     _context.Players.Add(player);
+        //     await _context.SaveChangesAsync();
+
+        //     return CreatedAtAction("GetPlayer", new { id = player.Id }, player);
+        // }
+
+        // DELETE: api/Player/5
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeletePlayer(int id)
+        // {
+        //     var player = await _context.Players.FindAsync(id);
+        //     if (player == null)
+        //     {
+        //         return NotFound();
+        //     }
+
+        //     _context.Players.Remove(player);
+        //     await _context.SaveChangesAsync();
+
+        //     return NoContent();
+        // }
+
+        // private bool PlayerExists(int id)
+        // {
+        //     return _context.Players.Any(e => e.Id == id);
+        // }
     }
+}
