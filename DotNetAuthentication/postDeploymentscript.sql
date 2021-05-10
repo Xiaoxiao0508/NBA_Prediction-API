877c05e72a1fa357f6ab94e848ea25463116c6b0
dan933 <69749413+dan933@users.noreply.github.com>
2021-05-10 16:44:49 +1000

    Initial Commit

--- DotNetAuthentication/postDeploymentscript.sql
+++ DotNetAuthentication/postDeploymentscript.sql
@@ -1,6 +1,6 @@
 ﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
+Post-Deployment Script Template		
+-------------------------------------------------------------------------------------
  This file contains SQL statements that will be appended to the build script.		
  Use SQLCMD syntax to include a file in the post-deployment script.			
  Example:      :r .\myfile.sql								
